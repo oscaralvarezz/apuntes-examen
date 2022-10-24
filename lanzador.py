d7d4ffaf2b5514eaf437a4bf9dc9ba80e1f0e1db
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2022-10-24 19:12:55 +0200

    .
    .

--- lanzador.py
+++ lanzador.py
@@ -1,7 +1,13 @@
+from importlib.resources import path
+import sys
+sys.path(0,path) #sys.path.append
+
 """
 Módulo que sirve solo para iniciar el juego
 """
 
 from juego import jugar
 
-jugar()
+if __name__ == "__main__":
+    jugar()
+    
