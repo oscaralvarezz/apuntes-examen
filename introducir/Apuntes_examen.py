331dd21447f162fbeb68d6166b38d5c6346cb710
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2022-10-24 18:58:25 +0200

    .
    .

--- introducir/Apuntes_examen.py
+++ introducir/Apuntes_examen.py
@@ -1,3 +1,20 @@
+import sys 
+sys.path.insert(0, "C:\Users\oscar\OneDrive\Documentos\algoritmos\oo")
+
+
+'''Es una lista de directorios/carpetas donde Python busca los módulos cuando 
+realizamos un import'''
+#Lo necesitamos si o si para el examen.
+#sería sys.path(0,la ruta del archivo)
+#va en el main, y sin ponerlo es imposible que funcione.
+
+#NO USAR FOR de ahora en adelante.
+
+#en los bucles que crearemos. en else tenemos que llamar a la propia
+#función creada y hacerle cosas.
+
+#no usaremos apenas print, usaremos el return.
+
 """
 Módulo que gestiona todas las funcionalidades de introducción de datos
 """
---
+++ introducir/apuntes/main.py
@@ -0,0 +1,5 @@
+#aqui solo va:
+from lanzador import jugar
+if __name__=="__main__":
+    jugar()
+    
